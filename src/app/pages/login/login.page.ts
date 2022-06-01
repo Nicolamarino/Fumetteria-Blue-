fd3115a7f0368ec0c09575c7b4559766f13928f0
fede49 <federicademarco49@gmail.com>
2022-06-01 12:00:37 +0200

    completato task CD-99

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 import { UserDTO } from 'src/app/model/package/DTO/user-dto';
 import { LoginService } from '../login.service';
 
@@ -8,10 +9,16 @@ import { LoginService } from '../login.service';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  email:string;
-  password:string;
+  @Input('formControlName') email:string;
+  @Input('formControlName') password:string;
+  loginForm;
 
-  constructor(private loginService: LoginService) { }
+  constructor(private loginService: LoginService, private fb:FormBuilder) {
+    this.loginForm=this.fb.group({
+      email:['', [Validators.email, Validators.required]],
+      password:['', [Validators.required, Validators.pattern, Validators.minLength(4)]]
+    })
+   }
 autenticazione(){
   this.loginService.login(this.email, this.password).subscribe(resp=> {
     const user:UserDTO=resp;
